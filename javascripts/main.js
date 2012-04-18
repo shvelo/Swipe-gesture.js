fe7675da2b3bb7c5d93740b3afb6c1865115f627
Nick Shvelidze <shveloo@gmail.com>
2012-04-18 21:16:02 +0400

    Added demo

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,16 @@
-console.log('This would be the main JS file.');
+var target = document.getElementById('demo');
+swipe(target,{
+	onSwipeRight: function(){
+		target.innerHTML = 'Right';
+	},
+	onSwipeLeft: function(){
+		target.innerHTML = 'Left';
+	},
+	onSwipeDown: function(){
+		target.innerHTML = 'Down';
+	},
+	onSwipeUp: function(){
+		target.innerHTML = 'Up';
+	},
+	minDistance: 0
+});
